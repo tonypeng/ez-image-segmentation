4ca3ace219ce58a7227a3c4e86cb80ae8a138150
Tony Peng <tonypeng@lightspeed.mit.edu>
2017-12-05 01:28:30 -0500

    complete ade loader

--- data_pipelines.py
+++ data_pipelines.py
@@ -44,15 +44,16 @@ class BatchedDataLoader:
             self._initialize_dataset()
 
         batch_x_file_paths = [None] * n
+        batch_y_file_paths = [None] * n
 
         for i in range(n):
-            batch_x_file_paths[i] = self.data_file_paths[self.data_idx]
+            batch_x_file_paths[i], batch_y_file_paths[i] = self.data_file_paths[self.data_idx]
             self.data_idx += 1
             if self.data_idx == len(self.data_file_paths):
                 self.data_idx = 0
                 shuffle(self.data_file_paths)
 
-        return batch_x_file_paths
+        return batch_x_file_paths, batch_y_file_paths
 
     def _initialize_dataset(self):
         if self.phase == BatchedDataLoader._TRAIN:
@@ -64,31 +65,50 @@ class BatchedDataLoader:
         raise NotImplementedError
 
 
-class Coco:
-    def __init__(self, data_root):
-        self.data_root = data_root
-
-    def get_train_file_paths(self):
-        return [os.path.join(self.data_root, f) for f in os.listdir(self.data_root)]
-
-    def get_valid_file_paths(self):
-        pass
-
-    def get_test_file_paths(self):
-        pass
-
-
 class MitAde:
     def __init__(self, data_root):
-        self.data_root = data_root
+        self.data_root = os.path.join(data_root, 'mitade')
 
     def get_train_file_paths(self):
-        pass
+        train_data_root_x = os.path.join(self.data_root, 'images', 'training')
+        train_data_root_y = os.path.join(self.data_root, 'annotations', 'training')
+        train_data_filepaths_x = []
+        train_data_filepaths_y = []
+        for f in os.listdir(train_data_root_x):
+            if not os.path.exists(os.path.join(train_data_root_y, f)):
+                print("Warning: Annotation doesn't exist for file ", f, ".")
+                continue
+            train_data_filepaths_x.append(os.path.join(train_data_root_x, f))
+            train_data_filepaths_y.append(os.path.join(train_data_root_y, f))
+
+        return train_data_filepaths_x, train_data_filepaths_y
 
     def get_valid_file_paths(self):
-        pass
+        valid_data_root_x = os.path.join(self.data_root, 'images', 'validation')
+        valid_data_root_y = os.path.join(self.data_root, 'annotations', 'validation')
+        valid_data_filepaths_x = []
+        valid_data_filepaths_y = []
+        for f in os.listdir(valid_data_root_x):
+            if not os.path.exists(os.path.join(valid_data_root_y, f)):
+                print("Warning: Annotation doesn't exist for file ", f, ".")
+                continue
+            valid_data_filepaths_x.append(os.path.join(valid_data_root_x, f))
+            valid_data_filepaths_y.append(os.path.join(valid_data_root_y, f))
+
+        return valid_data_filepaths_x, valid_data_filepaths_y
 
     def get_test_file_paths(self):
-        pass
+        test_data_root_x = os.path.join(self.data_root, 'images', 'test')
+        test_data_root_y = os.path.join(self.data_root, 'annotations', 'test')
+        test_data_filepaths_x = []
+        test_data_filepaths_y = []
+        for f in os.listdir(test_data_root_x):
+            if not os.path.exists(os.path.join(test_data_root_y, f)):
+                print("Warning: Annotation doesn't exist for file ", f, ".")
+                continue
+            test_data_filepaths_x.append(os.path.join(test_data_root_x, f))
+            test_data_filepaths_y.append(os.path.join(test_data_root_y, f))
+
+        return test_data_filepaths_x, test_data_filepaths_y
 
 
