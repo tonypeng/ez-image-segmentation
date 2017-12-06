fix accuracy computation

Trainer.py
@@ -67,7 +67,8 @@ class Trainer:
                 regularizer = tf.add_n(tf.get_collection('weight_regularizers'))
                 loss += regularizer
 
-            acc_per_pixel = tf.reduce_mean(tf.cast(tf.equal(tf.cast(preds, tf.int32), y), tf.float32))
+            pixels_equal = tf.equal(tf.squeeze(tf.cast(preds, tf.int32)), tf.squeeze(y))
+            acc_per_pixel = tf.reduce_mean(tf.cast(pixels_equal), tf.float32)
 
             # Create optimizer
             print("4. Optimizing...")